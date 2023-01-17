6b65ce83427d1a8f3013dd0c12428539247aef61
el3m3nTik <AVSitnikov@stud.kantiana.ru>
2023-01-17 17:08:27 +0200

    made changes lab3

--- lab3.py
+++ lab3.py
@@ -1,7 +1,12 @@
-x = 50
-for i in range(1, x):
-    for x in range(0, x):
-        for y in range(0, x):
-            for z in range(0, x):
-                if 3**x * 5**y * 7**z == i:
-                    print(i)
+import math
+x = 10000000
+count = 0
+for y in range(1, int(math.log(x))):
+    for z in range(0, int(math.log(x))):
+        for u in range(0, int(math.log(x))):
+            if (3**y * 5**z * 7**u) < x:
+                print(3**y * 5**z * 7**u)
+                count += 1
+            else:
+                break
+print(count)
